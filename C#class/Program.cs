41fbb4c4965f4d5b3e6582986aea4faddc012400
Muhydeen Adebayo <adediranma@examroom.ai>
2024-04-27 13:07:17 +0100

    Algorithm

--- C#class/Program.cs
+++ C#class/Program.cs
@@ -24,3 +24,16 @@ Console.WriteLine(muhydeen.MyName(muhydeen.FirstName, muhydeen.LastName));
 Console.WriteLine(muhydeen.MyName(muhydeen.FirstName, muhydeen.LastName, muhydeen.MiddleName));
 
 Console.WriteLine(muhydeen.MyName(muhydeen.FirstName, muhydeen.LastName, muhydeen.MiddleName, muhydeen.Title));
+
+
+// Division /
+// Mutiplication *
+// Modulous %
+
+//Example: Let get the remainder of this division
+
+var x = 8 % 5;
+var y = 8 / 5;
+var z = 8 * 5;
+
+Console.WriteLine($"Modulation give {x} while division gives {y} and multiplication gives {z}");
